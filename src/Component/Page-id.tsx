76b56bebd5b29f72e4a2dbd25f4d968b9e002649
stoclet sylvain <cronosu@hotmail.fr>
2023-10-09 19:16:19 +0200

    correction
    component 404 n'est plus visible au chargement de la page -id des logements

--- src/Component/Page-id.tsx
+++ src/Component/Page-id.tsx
@@ -3,30 +3,39 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import LogementID from "./LogementID.tsx"
 import Error404 from './Error404.tsx';
-import  logements  from '../../public/logements.json';
+import logements from '../../public/logements.json';
 
 function PageID() {
   const [donnees, setDonnees] = useState<any[]>([]);
   let location = useLocation();
 
- 
-  useEffect(() => {
-    setDonnees(logements); 
-  }, []);   
-  
-  const elementTrouve = donnees.find(e => "/" + e.id === location.pathname);
+  const [showError, setShowError] = useState(false);
 
+  useEffect(() => {
+    setDonnees(logements);
+  }, []);
+
+  const elementTrouve = donnees.find(e => "/" + e.id === location.pathname);
+  useEffect(() => {
+
+    if (!elementTrouve) {
+      const timeout = setTimeout(() => {
+        setShowError(true);
+      }, 150);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [elementTrouve]);
 
 
   return (
-    <div> 
-      {
-        elementTrouve ? (<>
-          <LogementID />  </>
-        ) : (<>
-        <Error404 />
-        </>
-        )}
+    <div>
+      {elementTrouve ? (
+        <LogementID />
+      ) : null}
+
+
+      {showError && <Error404 />}
     </div>
   );
 }
